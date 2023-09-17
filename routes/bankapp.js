9d158c2df4448410318e28de7f133db802725322
Gjmattia <Gjmattia@gmail.com>
2023-09-16 21:53:51 -0400

    added route, controller, ejs page for AboutBank app page, basically just a rea dme that is visible on the website

---
+++ routes/bankapp.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const router = express.Router();
+const bankAppCtrl = require('../controllers/bankapp');
+
+
+
+router.get('/', bankAppCtrl.index);
+
+
+module.exports = router;
--- server.js
+++ server.js
@@ -16,6 +16,7 @@ const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
 const accountsRouter = require('./routes/accounts');
 const beneficiariesRouter = require('./routes/beneficiaries');
+const bankAppRouter = require('./routes/bankapp');
 
 
 const app = express();
@@ -50,13 +51,14 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/accounts', accountsRouter);
 app.use('/beneficiaries', beneficiariesRouter);
+app.use('/bankapp', bankAppRouter);
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
